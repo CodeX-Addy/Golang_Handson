29840579486789e950d59e076640a42c379a945a
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-10-24 15:15:29 +0530

    .

--- Variables/main.go
+++ Variables/main.go
@@ -13,4 +13,10 @@ func main() {
 	var check bool = true;
 	fmt.Println(check);
 	fmt.Printf("The type of the variable check is: %T\n", check);
+
+	//Integer types
+	//We can simply use int as well
+	var num uint8 = 255; //Highest permissable value for uint8
+	fmt.Println(num);
+	fmt.Printf("The type of the variable num is: %T\n", num);
 }
