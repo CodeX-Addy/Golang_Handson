e64385c71f4a484be6dfb0ba3aa537dd93f7f558
CodeX-Addy <aditya.tomar9055049@gmail.com>
2024-10-24 15:03:42 +0530

    .

--- Variables/main.go
+++ Variables/main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
+
+	//String type
 	var user string = "Aditya";
 	fmt.Println(user);
-	fmt.Printf("The type of the variable is: %T\n", user);
+	fmt.Printf("The type of the variable user is: %T\n", user);
+
+	//Boolean type
+	var check bool = true;
+	fmt.Println(check);
+	fmt.Printf("The type of the variable check is: %T\n", check);
 }
