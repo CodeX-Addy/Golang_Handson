0d984d72fe77cc6e8215dc23faf09b9905e84c4a
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-01-13 13:19:21 +0530

    add:functions in go

--- Functions/Functions.go
+++ Functions/Functions.go
@@ -11,8 +11,14 @@ func main(){
 	
 	a, b:= 10, 20;
 
-	fmt.Println("Before swapping: ", a, b);
+	fmt.Println("Before swapping:", a, b);
 	a, b = swap(a, b);
-	fmt.Println("After swapping: ", a, b);
+	fmt.Println("After swapping:", a, b);
 
+	//Anonymous func declaration
+	add:= func(a, b int) int{
+		return a+b;
+	}
+
+	fmt.Println("Sum of 2 and 3 is:", add(2,3));
 }
