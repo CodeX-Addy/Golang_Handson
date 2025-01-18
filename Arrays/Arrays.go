610d86916a03d0fd27997080df185910a2e7020b
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-01-18 22:24:58 +0530

    .

--- Arrays/Arrays.go
+++ Arrays/Arrays.go
@@ -7,10 +7,8 @@ func main(){
 	arr := [5] int {1,2,3,4,5};
 	fmt.Println(arr);
 
-	//Slice
-	slice := [] int {1,2,3,4,5};
+	//Slice 
+	slice := []int{1,2,3,4,5};
 	slice = append(slice, 6);
 	fmt.Println("Sliced:", slice);
-
-	
 }
