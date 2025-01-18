75d750026aa26e464b2d08db8470883c1b5469db
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-01-18 22:52:47 +0530

    add: goroutines

--- Goroutines/Goroutines.go
+++ Goroutines/Goroutines.go
@@ -7,13 +7,13 @@ import (
 
 func printNumbers() {
     for i := 1; i <= 5; i++ {
-        fmt.Println(i)
-        time.Sleep(500 * time.Millisecond)
+        fmt.Println(i);
+        time.Sleep(500 * time.Millisecond);
     }
 }
 
 func main() {
-    go printNumbers()
-    fmt.Println("Goroutine started")
-    time.Sleep(3 * time.Second)
+    go printNumbers();
+    fmt.Println("Goroutine started");
+    time.Sleep(3 * time.Second);
 }
