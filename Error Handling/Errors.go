5843bcb304ebbbe375461623a5631e09317ef19a
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-01-18 22:55:29 +0530

    add: error handling

--- Error Handling/Errors.go
+++ Error Handling/Errors.go
@@ -7,7 +7,7 @@ import (
 
 func divide(a, b int) (int, error) {
     if b == 0 {
-        return 0, errors.New("division by zero")
+        return 0, errors.New("Division by zero..")
     }
     return a / b, nil
 }
