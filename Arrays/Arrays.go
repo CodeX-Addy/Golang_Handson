d74e99fffd3a95a01ef81d07cf2a77d829f4f977
CodeX-Addy <aditya.tomar9055049@gmail.com>
2025-01-18 22:27:43 +0530

    add: arrays, slice and map

--- Arrays/Arrays.go
+++ Arrays/Arrays.go
@@ -11,4 +11,9 @@ func main(){
 	slice := []int{1,2,3,4,5};
 	slice = append(slice, 6);
 	fmt.Println("Sliced:", slice);
+
+	//Map
+	scores := map[string]int{"A":10, "B":20};
+	scores["C"] = 30;
+	fmt.Println("Scores:", scores);
 }
